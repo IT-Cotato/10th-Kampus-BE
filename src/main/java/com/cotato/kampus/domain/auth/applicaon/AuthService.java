bd6b6cbc23e44f7b71ceb20314a953d119127ea6
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-01-03 22:51:24 +0900

    feat(#3): Add AuthService for Signup logic

---
+++ src/main/java/com/cotato/kampus/domain/auth/applicaon/AuthService.java
@@ -0,0 +1,26 @@
+package com.cotato.kampus.domain.auth.applicaon;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.cotato.kampus.domain.user.application.UserAppender;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class AuthService {
+
+	private final UserAppender userAppender;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public Long signup(String email, String password, String username, String nickname, String nationality) {
+		return userAppender.appendUser(
+			email,
+			bCryptPasswordEncoder.encode(password),
+			username,
+			nickname,
+			nationality
+		);
+	}
+}
