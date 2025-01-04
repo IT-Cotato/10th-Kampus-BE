921bceb8293b12447bcf09fabbbe28d8410342e6
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-01-04 14:36:55 +0900

    refactor(#3): Refactor User(add uniqueId, providerId)

--- src/main/java/com/cotato/kampus/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/cotato/kampus/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package com.cotato.kampus.domain.auth.dto.request;
 
 public record SignupRequest(
 	String email,
+	String uniqueId,
+	String providerId,
 	String username,
 	String nickname,
 	String password,
