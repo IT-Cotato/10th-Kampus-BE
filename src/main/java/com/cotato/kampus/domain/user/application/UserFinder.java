464e5c31f4ce17b437c6dc5848e3247eaeedaf3f
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-01-04 23:34:51 +0900

    feat(#8): Add ApiUserResolver for getting user info from SecurityContextHolder

---
+++ src/main/java/com/cotato/kampus/domain/user/application/UserFinder.java
@@ -0,0 +1,23 @@
+package com.cotato.kampus.domain.user.application;
+
+import org.springframework.stereotype.Component;
+
+import com.cotato.kampus.domain.user.dao.UserRepository;
+import com.cotato.kampus.domain.user.domain.User;
+import com.cotato.kampus.global.error.ErrorCode;
+import com.cotato.kampus.global.error.exception.AppException;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserFinder {
+
+	private final UserRepository userRepository;
+
+	public User findByUniqueId(String uniqueId) {
+		return userRepository.findByUniqueId(uniqueId)
+			.orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+	}
+}
