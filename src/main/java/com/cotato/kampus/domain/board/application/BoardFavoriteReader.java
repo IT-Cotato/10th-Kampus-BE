c5b43e022746543598f4b7fdd6c9a8bf934082ee
u-genuine <u_genuine@naver.com>
2025-01-05 10:47:07 +0900

    feat: Create BoardFavoriteRepository and BoardFavoriteReader

---
+++ src/main/java/com/cotato/kampus/domain/board/application/BoardFavoriteReader.java
@@ -0,0 +1,33 @@
+package com.cotato.kampus.domain.board.application;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cotato.kampus.domain.board.dao.BoardFavoriteRepository;
+import com.cotato.kampus.domain.board.domain.BoardFavorite;
+import com.cotato.kampus.domain.common.application.ApiUserResolver;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@Transactional(readOnly = true)
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardFavoriteReader {
+
+	private final BoardFavoriteRepository boardFavoriteRepository;
+	private final ApiUserResolver apiUserResolver;
+
+	@Transactional
+	public Set<Long> read() {
+		Long userId = apiUserResolver.getUserId();
+		return boardFavoriteRepository.findAllByUserId(userId)
+			.stream()
+			.map(BoardFavorite::getBoardId)
+			.collect(Collectors.toSet());
+	}
+}
