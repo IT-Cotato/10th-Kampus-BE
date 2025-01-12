6cdb1c8187be214b1dea0eb152d27e3934c5d754
u-genuine <u_genuine@naver.com>
2025-01-12 19:59:09 +0900

    feat: Add PostDeleter, PostFinder

---
+++ src/main/java/com/cotato/kampus/domain/post/application/PostFinder.java
@@ -0,0 +1,24 @@
+package com.cotato.kampus.domain.post.application;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cotato.kampus.domain.post.dao.PostRepository;
+import com.cotato.kampus.domain.post.domain.Post;
+import com.cotato.kampus.global.error.ErrorCode;
+import com.cotato.kampus.global.error.exception.AppException;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@Transactional(readOnly = true)
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostFinder {
+	private final PostRepository postRepository;
+
+	public Post getById(Long postId){
+		return postRepository.findById(postId)
+			.orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND));
+	}
+}
