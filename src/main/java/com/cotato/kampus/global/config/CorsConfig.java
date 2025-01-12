ef3bb0a75c0b52cbf4a48e1e1ac6774618d1e45a
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-01-12 16:09:13 +0900

    feat(#25): Add cors configuration

---
+++ src/main/java/com/cotato/kampus/global/config/CorsConfig.java
@@ -0,0 +1,41 @@
+package com.cotato.kampus.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+	private static final String[] ORIGINS = {
+		"http://localhost:3000",
+		"http://54.180.123.60:8080",
+		"https://kampus.kro.kr"
+	};
+
+	private static final String[] ALLOWED_HEADERS = {
+		"Authorization",
+		"Refresh-Token"
+	};
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(List.of(ORIGINS));
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.setExposedHeaders(List.of(ALLOWED_HEADERS));
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
+	}
+}
