c0350da1e2c6a7870892732d6d7673904e89494f
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-01-12 17:30:59 +0900

    chore(#29): Add Swagger description in AuthController

--- src/main/java/com/cotato/kampus/domain/auth/api/AuthController.java
+++ src/main/java/com/cotato/kampus/domain/auth/api/AuthController.java
@@ -12,9 +12,14 @@ import com.cotato.kampus.domain.auth.dto.request.SignupRequest;
 import com.cotato.kampus.domain.auth.dto.response.SignupResponse;
 import com.cotato.kampus.global.common.dto.DataResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "인증/인가", description = "인증/인가 관련 API")
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping("/v1/api/auth")
@@ -23,21 +28,16 @@ public class AuthController {
 	private final AuthService authService;
 
 	@PostMapping("/signup")
-	public ResponseEntity<DataResponse<SignupResponse>> signup(@RequestBody SignupRequest request) {
-		return ResponseEntity.ok(DataResponse.from(
-				SignupResponse.of(
-					authService.signup(
-						request.email(),
-						request.uniqueId(),
-						request.providerId(),
-						request.username(),
-						request.nickname(),
-						request.nationality())
-				)
-			)
-		);
+	@Operation(summary = "일반 로그인", description = "Client 단에서 소셜 로그인 후 회원가입 요청하는 api")
+	public ResponseEntity<DataResponse<SignupResponse>> signup(
+		@Parameter(description = "회원가입 요청 정보", schema = @Schema(implementation = SignupRequest.class))
+		@RequestBody SignupRequest request) {
+		return ResponseEntity.ok(DataResponse.from(SignupResponse.of(
+			authService.signup(request.email(), request.uniqueId(), request.providerId(), request.username(),
+				request.nickname(), request.nationality()))));
 	}
 
+	@Operation(summary = "서버 헬스 체크", description = "서버 헬스 체크")
 	@GetMapping("/health")
 	public ResponseEntity<DataResponse<Void>> health() {
 		return ResponseEntity.ok(DataResponse.ok());
