d94e2a319023899997afe5efab849cfe1f718172
u-genuine <u_genuine@naver.com>
2025-01-12 11:50:22 +0900

    refactor: Remove @Builder

--- src/main/java/com/cotato/kampus/domain/board/domain/UniversityBoard.java
+++ src/main/java/com/cotato/kampus/domain/board/domain/UniversityBoard.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,7 +16,6 @@ public class UniversityBoard extends Board {
 	@Column(name = "university_id", nullable = false)
 	private Long universityId;
 
-	@Builder
 	public UniversityBoard(Long universityId, String boardName) {
 		super(boardName);
 		this.universityId = universityId;
