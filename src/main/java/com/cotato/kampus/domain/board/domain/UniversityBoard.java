5fdc643296cd737fb2775001e3db32344d818c1e
u-genuine <u_genuine@naver.com>
2025-01-12 11:29:52 +0900

    refactor: Extend Board class in UniversityBoard

--- src/main/java/com/cotato/kampus/domain/board/domain/UniversityBoard.java
+++ src/main/java/com/cotato/kampus/domain/board/domain/UniversityBoard.java
@@ -1,12 +1,7 @@
 package com.cotato.kampus.domain.board.domain;
 
-import com.cotato.kampus.domain.common.domain.BaseTimeEntity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -14,24 +9,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "universirt_board")
+@Table(name = "university_board")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UniversityBoard extends BaseTimeEntity {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class UniversityBoard extends Board {
 
 	@Column(name = "university_id", nullable = false)
 	private Long universityId;
 
-	@Column(name = "title", nullable = false)
-	private String boardName;
-
 	@Builder
 	public UniversityBoard(Long universityId, String boardName) {
+		super(boardName);
 		this.universityId = universityId;
-		this.boardName = boardName;
 	}
 }
