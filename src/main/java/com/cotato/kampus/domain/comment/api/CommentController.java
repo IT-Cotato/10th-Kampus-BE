e04b0ae24dec7e4cb99826f177c14be7f139643e
u-genuine <u_genuine@naver.com>
2025-01-21 13:38:41 +0900

    feat: Add Swagger documentation

--- src/main/java/com/cotato/kampus/domain/comment/api/CommentController.java
+++ src/main/java/com/cotato/kampus/domain/comment/api/CommentController.java
@@ -17,9 +17,12 @@ import com.cotato.kampus.domain.comment.dto.response.CommentLikeResponse;
 import com.cotato.kampus.domain.comment.dto.response.CommentListResponse;
 import com.cotato.kampus.global.common.dto.DataResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "댓글(Comment) API", description = "댓글 관련 API")
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping("/v1/api")
@@ -28,6 +31,7 @@ public class CommentController {
 	private final CommentService commentService;
 
 	@PostMapping("/posts/{postId}/comments")
+	@Operation(summary = "댓글 작성", description = "특정 게시글에 댓글을 추가합니다.")
 	public ResponseEntity<DataResponse<CommentCreateResponse>> createComment(
 		@PathVariable Long postId,
 		@RequestBody CommentCreateRequest request){
@@ -46,6 +50,7 @@ public class CommentController {
 	}
 
 	@DeleteMapping("/comments/{commentId}")
+	@Operation(summary = "댓글 삭제", description = "특정 댓글을 삭제합니다.")
 	public ResponseEntity<DataResponse<CommentDeleteResponse>> deleteComment(
 		@PathVariable Long commentId
 	){
@@ -61,6 +66,7 @@ public class CommentController {
 	}
 
 	@PostMapping("/comments/{commentId}/like")
+	@Operation(summary = "댓글 좋아요 추가", description = "특정 댓글에 좋아요를 추가합니다.")
 	public ResponseEntity<DataResponse<CommentLikeResponse>> toggleLikeForComment(
 		@PathVariable Long commentId
 	){
@@ -76,6 +82,7 @@ public class CommentController {
 	}
 
 	@GetMapping("/posts/{postId}/comments")
+	@Operation(summary ="댓글 조회", description = "특정 게시글의 모든 댓글과 대댓글을 조회합니다.")
 	public ResponseEntity<DataResponse<CommentListResponse>> getPostComments(
 		@PathVariable Long postId
 	){
