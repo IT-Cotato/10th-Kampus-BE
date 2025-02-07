60ca6e654994acf878434ab0477ab5af4ead5e69
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-02-07 21:20:02 +0900

    feat(#69): 유저 정보 조회 api 구현

--- src/main/java/com/cotato/kampus/domain/user/application/UserService.java
+++ src/main/java/com/cotato/kampus/domain/user/application/UserService.java
@@ -3,6 +3,8 @@ package com.cotato.kampus.domain.user.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cotato.kampus.domain.common.application.ApiUserResolver;
+import com.cotato.kampus.domain.user.dto.UserDetailsDto;
 import com.cotato.kampus.domain.user.enums.Nationality;
 import com.cotato.kampus.domain.user.enums.PreferredLanguage;
 
@@ -15,6 +17,11 @@ public class UserService {
 
 	private final UserUpdater userUpdater;
 	private final UserValidator userValidator;
+	private final ApiUserResolver apiUserResolver;
+
+	public UserDetailsDto getUserDetails() {
+		return UserDetailsDto.from(apiUserResolver.getUser());
+	}
 
 	@Transactional
 	public Long updateUserDetails(String nickname, Nationality nationality, PreferredLanguage preferredLanguage) {
