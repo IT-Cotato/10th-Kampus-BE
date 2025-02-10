07b92601d3d4f9c091f68943b83d5d13f8198c19
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-02-10 19:43:22 +0900

    refactor(#74): 유저 정보 조회 api에 유저상태에 따라 needSetup 추가

--- src/main/java/com/cotato/kampus/domain/user/dto/response/UserDetailsResponse.java
+++ src/main/java/com/cotato/kampus/domain/user/dto/response/UserDetailsResponse.java
@@ -3,19 +3,22 @@ package com.cotato.kampus.domain.user.dto.response;
 import com.cotato.kampus.domain.user.dto.UserDetailsDto;
 import com.cotato.kampus.domain.user.enums.Nationality;
 import com.cotato.kampus.domain.user.enums.PreferredLanguage;
+import com.cotato.kampus.domain.user.enums.UserStatus;
 
 public record UserDetailsResponse(
 	Long id,
 	String nickname,
 	Nationality nationality,
-	PreferredLanguage preferredLanguage
+	PreferredLanguage preferredLanguage,
+	boolean needSetup
 ) {
 	public static UserDetailsResponse from(UserDetailsDto userDetails) {
 		return new UserDetailsResponse(
 			userDetails.id(),
 			userDetails.nickname(),
 			userDetails.nationality(),
-			userDetails.preferredLanguage()
+			userDetails.preferredLanguage(),
+			userDetails.userStatus().equals(UserStatus.PENDING_DETAILS)
 		);
 	}
 }
