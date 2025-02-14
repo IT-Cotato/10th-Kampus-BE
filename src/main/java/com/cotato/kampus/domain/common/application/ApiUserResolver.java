09b3470076313173f110de00c27a186ba4e0b94a
u-genuine <u_genuine@naver.com>
2025-02-14 13:38:25 +0900

    refactor: 안쓰이는 코드 삭제

--- src/main/java/com/cotato/kampus/domain/common/application/ApiUserResolver.java
+++ src/main/java/com/cotato/kampus/domain/common/application/ApiUserResolver.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.cotato.kampus.domain.user.application.UserFinder;
-import com.cotato.kampus.domain.user.dao.UserRepository;
 import com.cotato.kampus.domain.user.domain.User;
 import com.cotato.kampus.global.auth.nativeapp.AppUserDetails;
 import com.cotato.kampus.global.error.ErrorCode;
@@ -20,7 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 public class ApiUserResolver {
 
 	private final UserFinder userFinder;
-	private final UserRepository userRepository;
 
 	public Long getUserId() {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
