dc6d0c03f75a7eb3859d0210318a41b56cd629ae
u-genuine <u_genuine@naver.com>
2025-02-20 18:37:26 +0900

    Style: 포매팅 적용

--- src/main/java/com/cotato/kampus/domain/board/dto/response/BoardListResponse.java
+++ src/main/java/com/cotato/kampus/domain/board/dto/response/BoardListResponse.java
@@ -7,7 +7,7 @@ import com.cotato.kampus.domain.board.dto.BoardWithFavoriteStatus;
 public record BoardListResponse(
 	List<BoardWithFavoriteStatus> boards
 ) {
-	public static BoardListResponse from(List<BoardWithFavoriteStatus> boards){
+	public static BoardListResponse from(List<BoardWithFavoriteStatus> boards) {
 		return new BoardListResponse(boards);
 	}
 }
