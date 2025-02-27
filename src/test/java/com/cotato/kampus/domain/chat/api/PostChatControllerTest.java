c1681619e0b3572b1d61291da283420f98fdeacd
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-02-27 23:11:31 +0900

    test(#172): PostChatController 채팅방 생성 단위 테스트

---
+++ src/test/java/com/cotato/kampus/domain/chat/api/PostChatControllerTest.java
@@ -0,0 +1,67 @@
+package com.cotato.kampus.domain.chat.api;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.cotato.kampus.domain.chat.application.PostChatService;
+import com.cotato.kampus.domain.chat.dto.request.ChatroomRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(controllers = PostChatController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class PostChatControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@MockitoBean
+	private PostChatService postChatService;
+
+	@MockitoBean
+	private SimpMessagingTemplate messagingTemplate;
+
+	@BeforeEach
+	public void init() {
+
+	}
+
+	@Test
+	@DisplayName("채팅방 생성 API 테스트")
+	void createChatroomTest() throws Exception {
+		// given
+		Long postId = 1L;
+		ChatroomRequest request = new ChatroomRequest(postId);
+
+		given(postChatService.createChatRoom(postId)).willReturn(1L);
+
+		// when & then
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/v1/api/chats/post")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+			)
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andExpect(jsonPath("$.data.chatRoomId").value(1L))
+			.andReturn();
+	}
+}
