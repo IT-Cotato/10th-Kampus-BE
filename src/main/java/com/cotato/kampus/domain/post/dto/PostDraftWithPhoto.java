79a717de7046fdc839e63fd1d871732d582ce89b
u-genuine <u_genuine@naver.com>
2025-04-01 23:59:57 +0900

    Refactor(IT-Cotato#181): 임시저장글 조회 게시판 id 포함되도록 수정

--- src/main/java/com/cotato/kampus/domain/post/dto/PostDraftWithPhoto.java
+++ src/main/java/com/cotato/kampus/domain/post/dto/PostDraftWithPhoto.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record PostDraftWithPhoto(
 	Long draftId,
+	Long boardId,
 	String title,
 	String content,
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -17,6 +18,7 @@ public record PostDraftWithPhoto(
 	public static PostDraftWithPhoto from(PostDraft postDraft, PostDraftPhoto postDraftPhoto) {
 		return new PostDraftWithPhoto(
 			postDraft.getId(),
+			postDraft.getBoardId(),
 			postDraft.getTitle(),
 			postDraft.getContent(),
 			postDraft.getCreatedTime(),
