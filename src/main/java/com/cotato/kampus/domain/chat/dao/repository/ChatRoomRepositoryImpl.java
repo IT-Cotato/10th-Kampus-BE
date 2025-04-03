ce6a17c3a17e7e62a8683f61485d1822de9285a3
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-04-03 20:26:39 +0900

    refactor(#184): chatRoomRepository 인터페이스화

---
+++ src/main/java/com/cotato/kampus/domain/chat/dao/repository/ChatRoomRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.cotato.kampus.domain.chat.dao.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Repository;
+
+import com.cotato.kampus.domain.chat.dao.entity.ChatRoomEntity;
+import com.cotato.kampus.domain.chat.domain.ChatRoom;
+import com.cotato.kampus.domain.chat.implement.chatroom.port.ChatRoomRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoomRepositoryImpl implements ChatRoomRepository {
+
+	private final ChatRoomJpaRepository chatRoomJpaRepository;
+
+	@Override
+	public Optional<ChatRoom> findById(Long chatroomId) {
+		return chatRoomJpaRepository.findById(chatroomId).map(ChatRoomEntity::toDomain);
+	}
+
+	@Override
+	public boolean existsByPostIdAndInitialSenderId(Long postId, Long senderId) {
+		return chatRoomJpaRepository.existsByPostIdAndInitialSenderId(postId, senderId);
+	}
+
+	@Override
+	public Slice<ChatRoom> findAllByUserIdOrderByCreatedTimeDesc(Long userId, Pageable pageable) {
+		return chatRoomJpaRepository.findAllByUserIdOrderByCreatedTimeDesc(userId, pageable)
+			.map(ChatRoomEntity::toDomain);
+	}
+}
