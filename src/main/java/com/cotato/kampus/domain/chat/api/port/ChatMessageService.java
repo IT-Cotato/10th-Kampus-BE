743e0394e9e12ad1f9a8d3020c2c8c49a5a6ab99
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-04-03 15:22:59 +0900

    feat(#184): ChatMessageService 인터페이스 생성

---
+++ src/main/java/com/cotato/kampus/domain/chat/api/port/ChatMessageService.java
@@ -0,0 +1,12 @@
+package com.cotato.kampus.domain.chat.api.port;
+
+import com.cotato.kampus.domain.chat.domain.ChatMessageSliceSnapshot;
+import com.cotato.kampus.domain.chat.domain.ChatNotificationResult;
+
+public interface ChatMessageService {
+
+	public ChatNotificationResult processNewMessage(Long chatroomId, String message);
+
+	public ChatMessageSliceSnapshot getMessages(int page, Long chatroomId);
+
+}
