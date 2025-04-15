087b573eb118713e262cc647c421fc889d9ca1d2
u-genuine <u_genuine@naver.com>
2025-04-15 22:33:18 +0900

    refactor(IT-Cotato#185): Enum 필드 @NotNull로 변경

--- src/main/java/com/cotato/kampus/domain/admin/dto/request/BoardCreateRequest.java
+++ src/main/java/com/cotato/kampus/domain/admin/dto/request/BoardCreateRequest.java
@@ -5,12 +5,14 @@ import java.util.List;
 import com.cotato.kampus.domain.board.enums.BoardType;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record BoardCreateRequest(
 	@NotBlank
 	String boardName,
 	@NotBlank
 	String description,
+	@NotNull
 	BoardType boardType,
 	String universityCode,
 	List<String> categories
