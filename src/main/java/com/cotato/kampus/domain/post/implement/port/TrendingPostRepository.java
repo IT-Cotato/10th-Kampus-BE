2de249329fc26962df214441494c0f59176c98ee
u-genuine <u_genuine@naver.com>
2025-04-24 14:21:38 +0900

    rename(IT-Cotato#186): TrendingPostRepository 인터페이스화

--- src/main/java/com/cotato/kampus/domain/post/implement/port/TrendingPostRepository.java
+++ src/main/java/com/cotato/kampus/domain/post/implement/port/TrendingPostRepository.java
@@ -2,17 +2,15 @@ package com.cotato.kampus.domain.post.implement.port;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import com.cotato.kampus.domain.post.domain.TrendingPost;
 
-@Repository
-public interface TrendingPostRepository extends JpaRepository<TrendingPost, Long> {
+public interface TrendingPostRepository {
+
+	TrendingPost save(TrendingPost trendingPost);
+
+	boolean existsByPostId(Long postId);
 
 	void deleteByPostId(Long postId);
 
-	@Query("SELECT t.postId FROM TrendingPost t")
 	List<Long> findAllPostIds();
 }
