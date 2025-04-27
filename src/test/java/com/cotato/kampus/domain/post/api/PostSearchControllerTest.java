79f508de5bb43605edbada087c3dfe5532d3885b
u-genuine <u_genuine@naver.com>
2025-04-27 23:20:20 +0900

    test(IT-Cotato#186): PostController에서 PostSearchController로 테스트 클래스 변경

--- src/test/java/com/cotato/kampus/domain/post/api/PostSearchControllerTest.java
+++ src/test/java/com/cotato/kampus/domain/post/api/PostSearchControllerTest.java
@@ -14,27 +14,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.cotato.kampus.domain.board.application.BoardService;
 import com.cotato.kampus.domain.post.application.PostSearchService;
-import com.cotato.kampus.domain.post.application.PostService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-@WebMvcTest(PostController.class)
+@WebMvcTest(PostSearchController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
-class PostControllerTest {
+class PostSearchControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
-
 	@MockitoBean
 	private PostSearchService postSearchService;
-
-	@MockitoBean
-	private BoardService boardService;
 
 	@Test
 	@DisplayName("검색 기록 전체 삭제 성공 케이스")
