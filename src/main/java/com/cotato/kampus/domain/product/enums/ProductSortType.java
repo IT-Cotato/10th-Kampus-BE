688e2b61ae1bceac1901fa7d7f7e9d4fc386f9da
u-genuine <u_genuine@naver.com>
2025-05-18 14:48:01 +0900

    refactor(IT-Cotato#250): 오래된순 bumpedTime 기준으로 변경

--- src/main/java/com/cotato/kampus/domain/product/enums/ProductSortType.java
+++ src/main/java/com/cotato/kampus/domain/product/enums/ProductSortType.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ProductSortType {
 	recent("bumpedTime", Sort.Direction.DESC), // 최신순(끌어올린 시간)
-	old("createdTime", Sort.Direction.ASC), // 오래된순
+	old("bumpedTime", Sort.Direction.ASC), // 오래된순(끌어올린 시간)
 	scrapCount("scrapCount", Sort.Direction.DESC); // 스크랩 많은 순
 
 	private final String property;
