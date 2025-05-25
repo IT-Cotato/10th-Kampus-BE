7a3d6e8e10cbea0111e95ef6d0b46d40549248c9
u-genuine <u_genuine@naver.com>
2025-05-25 15:01:20 +0900

    test(IT-Cotato#252): 즐겨찾기 게시판 목록 조회 테스트 코드 작성
    - 즐겨찾게 게시판 미리보기 - 성공

--- src/test/java/com/cotato/kampus/domain/board/application/BoardServiceTest.java
+++ src/test/java/com/cotato/kampus/domain/board/application/BoardServiceTest.java
@@ -25,6 +25,10 @@ import com.cotato.kampus.domain.board.enums.BoardType;
 import com.cotato.kampus.domain.board.implement.port.BoardFavoriteRepository;
 import com.cotato.kampus.domain.board.implement.port.BoardRepository;
 import com.cotato.kampus.domain.common.application.ApiUserResolver;
+import com.cotato.kampus.domain.common.enums.Anonymity;
+import com.cotato.kampus.domain.post.domain.NormalPost;
+import com.cotato.kampus.domain.post.enums.PostStatus;
+import com.cotato.kampus.domain.post.implement.port.PostRepository;
 import com.cotato.kampus.domain.user.enums.UserRole;
 import com.cotato.kampus.helper.TestUserHelper;
 
@@ -44,6 +48,9 @@ class BoardServiceTest {
 	@Autowired
 	private BoardFavoriteRepository boardFavoriteRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	private Board savedBoard1;
 	private Board savedBoard2;
 	private Board savedBoard3;
@@ -52,7 +59,7 @@ class BoardServiceTest {
 
 	@BeforeEach
 	void setUp() {
-		savedBoard1 = boardRepository.save(NormalBoard.create("자유게시판", "자유게시판입니다", false,
+		savedBoard1 = boardRepository.save(NormalBoard.create("일반게시판", "자유게시판입니다", false,
 			BoardStatus.ACTIVE, BoardType.NORMAL));
 		savedBoard2 = boardRepository.save(NormalBoard.create("카드뉴스게시판", "카드뉴스게시판입니다", false,
 			BoardStatus.ACTIVE, BoardType.CARDNEWS));
@@ -61,14 +68,30 @@ class BoardServiceTest {
 		savedBoard4 = boardRepository.save(NormalBoard.create("트렌딩게시판", "트랜딩게시판입니다", false,
 			BoardStatus.ACTIVE, BoardType.TRENDING));
 		savedBoard5 = boardRepository.save(UniversityBoard.create("홍익대학교", "홍대생 전용 게시판입니다", false,
-			BoardStatus.ACTIVE, BoardType.UNIVERSITY, 2L));
+			BoardStatus.ACTIVE, BoardType.UNIVERSITY, 401L));
+
+		postRepository.save(
+			NormalPost.create(savedBoard1.getId(), 1L, "일반게시판 게시글", "내용1", PostStatus.PUBLISHED, Anonymity.ANONYMOUS)
+		);
+		postRepository.save(
+			NormalPost.create(savedBoard2.getId(), 2L, "카드뉴스게시판 게시글", "내용2", PostStatus.PUBLISHED, Anonymity.ANONYMOUS)
+		);
+		postRepository.save(
+			NormalPost.create(savedBoard3.getId(), 3L, "고정게시판 게시글", "내용3", PostStatus.PUBLISHED, Anonymity.ANONYMOUS)
+		);
+		postRepository.save(
+			NormalPost.create(savedBoard4.getId(), 4L, "트렌딩게시판 게시글", "내용4", PostStatus.PUBLISHED, Anonymity.ANONYMOUS)
+		);
+		postRepository.save(
+			NormalPost.create(savedBoard5.getId(), 5L, "홍익대학교 게시글", "내용5", PostStatus.PUBLISHED, Anonymity.ANONYMOUS)
+		);
 	}
 
 	@Test
 	@DisplayName("공용 게시판 목록 조회 테스트 - 성공")
 	void getBoardList_success() {
 		// Given
-		Long userId = TestUserHelper.createUserDto(1L, null, UserRole.VERIFIED).id();
+		Long userId = TestUserHelper.createUserDto(1L, null, UserRole.UNVERIFIED).id();
 		given(apiUserResolver.getCurrentUserId()).willReturn(userId);
 
 		// 트렌딩 게시판 즐겨찾기
@@ -88,16 +111,16 @@ class BoardServiceTest {
 	}
 
 	@Test
-	@DisplayName("즐겨찾는 게시판 미리보기 - 성공")
-	void getBoardFavorite_success() {
+	@DisplayName("즐겨찾기 게시판 미리보기 테스트 - 성공")
+	void getFavoriteBoardPreview_success() {
 		// Given
-		Long userId = TestUserHelper.createUserDto(1L, null, UserRole.VERIFIED).id();
+		Long userId = TestUserHelper.createUserDto(1L, null, UserRole.UNVERIFIED).id();
 		given(apiUserResolver.getCurrentUserId()).willReturn(userId);
 
 		// 고정 게시판 즐겨찾기
 		boardFavoriteRepository.save(BoardFavorite.builder()
 			.userId(userId)
-			.boardId(savedBoard4.getId())
+			.boardId(savedBoard3.getId())
 			.build());
 
 		// When
@@ -105,5 +128,7 @@ class BoardServiceTest {
 
 		// Then
 		assertThat(result.size()).isEqualTo(1);
+		assertThat(result.get(0).boardId()).isEqualTo(savedBoard3.getId());
+		assertThat(result.get(0).postTitle()).isEqualTo("고정게시판 게시글");
 	}
 }
