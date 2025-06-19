c8fa94a313fbb1a6b395195eb47ab3ac07a4c8da
u-genuine <u_genuine@naver.com>
2025-06-19 17:53:39 +0900

    feature(IT-Cotato#287): 재학생 서류인증 반려 사유 조회 구현

--- src/main/java/com/cotato/kampus/domain/verification/dao/VerificationRecordRepository.java
+++ src/main/java/com/cotato/kampus/domain/verification/dao/VerificationRecordRepository.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.cotato.kampus.domain.user.enums.VerificationStatus;
+import com.cotato.kampus.domain.user.enums.VerificationType;
 import com.cotato.kampus.domain.verification.domain.VerificationRecord;
 
 public interface VerificationRecordRepository extends JpaRepository<VerificationRecord, Long> {
@@ -15,4 +18,10 @@ public interface VerificationRecordRepository extends JpaRepository<Verification
 	Optional<VerificationRecord> findByUserId(Long userId);
 
 	void deleteAllByUserId(Long userId);
+
+	@Query("SELECT v FROM VerificationRecord v "
+		+ "WHERE v.userId = :userId "
+		+ "AND v.verificationType = :type "
+		+ "AND v.verificationStatus = :status")
+	Optional<VerificationRecord> findLatestByUserIdAndTypeAndStatus(Long userId, VerificationType type, VerificationStatus status);
 }
