92f10dd16869a6da9a298660296cd7232a90c79a
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-07-14 18:07:25 +0900

    fix: 쿠키 domain 설정

--- src/main/java/com/cotato/kampus/global/auth/util/CookieUtil.java
+++ src/main/java/com/cotato/kampus/global/auth/util/CookieUtil.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CookieUtil {
 
 	private static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+	private static final String DOMAIN = "kampusapp.kro.kr";
 	private static final int COOKIE_MAX_AGE = 60 * 60 * 24 * 3;
 
 	public static void setRefreshTokenCookie(HttpServletResponse response, final String refreshToken) {
@@ -19,6 +20,7 @@ public class CookieUtil {
 			.httpOnly(true)
 			.secure(true)
 			.path("/")
+			.domain(DOMAIN)
 			.maxAge(COOKIE_MAX_AGE)
 			.sameSite("None")
 			.build();
