49f80c830718b0a18cf85a3465d2839f2065f75b
u-genuine <u_genuine@naver.com>
2025-09-10 14:28:03 +0900

    refactor(#364): ProductDetailResponse를 ProductDetails 레코드로 래핑하도록 변경

--- src/main/java/com/cotato/kampus/domain/product/api/response/ProductDetailResponse.java
+++ src/main/java/com/cotato/kampus/domain/product/api/response/ProductDetailResponse.java
@@ -1,46 +1,11 @@
 package com.cotato.kampus.domain.product.api.response;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import com.cotato.kampus.domain.product.enums.ProductStatus;
 import com.cotato.kampus.domain.product.domain.ProductDetails;
-import com.cotato.kampus.domain.product.domain.ProductPhotoInfo;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record ProductDetailResponse(
-	Long productId,
-	Long sellerId,
-	String sellerName,
-	String title,
-	int price,
-	List<String> categories,
-	String description,
-	List<ProductPhotoInfo> photos,
-	ProductStatus productStatus,
-	int scrapCount,
-	int chatCount,
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	LocalDateTime createdTime,
-	boolean isAuthor,
-	boolean isScrapped
+	ProductDetails productDetails
 ) {
 	public static ProductDetailResponse from(ProductDetails productDetails) {
-		return new ProductDetailResponse(
-			productDetails.productId(),
-			productDetails.sellerId(),
-			productDetails.sellerName(),
-			productDetails.title(),
-			productDetails.price(),
-			productDetails.categories(),
-			productDetails.description(),
-			productDetails.photos(),
-			productDetails.postStatus(),
-			productDetails.scrapCount(),
-			productDetails.chatCount(),
-			productDetails.createdTime(),
-			productDetails.isAuthor(),
-			productDetails.isScrapped()
-		);
+		return new ProductDetailResponse(productDetails);
 	}
 }
