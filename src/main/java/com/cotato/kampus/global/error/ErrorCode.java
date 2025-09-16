bccf92e5a4d440f929005874d07c6c3cd8c5f840
u-genuine <u_genuine@naver.com>
2025-09-16 22:02:47 +0900

    fix(#372): CHATROOM_DUPLICATED 에러 코드 HTTP 상태를 CONFLICT로 변경

--- src/main/java/com/cotato/kampus/global/error/ErrorCode.java
+++ src/main/java/com/cotato/kampus/global/error/ErrorCode.java
@@ -156,7 +156,7 @@ public enum ErrorCode {
 
 	// Chat
 	INVALID_CHATROOM(HttpStatus.FORBIDDEN, "자신에게 채팅을 할 수 없습니다.", "CHAT-001"),
-	CHATROOM_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 존재하는 채팅 방입니다.", "CHAT-002"),
+	CHATROOM_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 채팅 방입니다.", "CHAT-002"),
 	CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 채팅방을 찾을 수 없습니다.", "CHAT-003"),
 	CHATROOM_NOT_ENTERED(HttpStatus.FORBIDDEN, "채팅방에 입장한 유저가 아닙니다.", "CHAT-004"),
 	READ_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 채팅 읽음 상태를 찾을 수 없습니다.", "CHAT-005"),
