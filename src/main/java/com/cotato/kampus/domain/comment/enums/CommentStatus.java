37e6db09aea16f98efcc6f65b594cc5826bd3bd9
u-genuine <u_genuine@naver.com>
2025-09-22 16:24:34 +0900

    refactor(#376): CommentStatus enum에 상태 판별 및 조회 메서드 추가

--- src/main/java/com/cotato/kampus/domain/comment/enums/CommentStatus.java
+++ src/main/java/com/cotato/kampus/domain/comment/enums/CommentStatus.java
@@ -1,9 +1,23 @@
 package com.cotato.kampus.domain.comment.enums;
 
+import java.util.Set;
+
 public enum CommentStatus {
 	NORMAL,
-	MASKED, // 마스킹 표시 (대댓글 떄문에 남겨둠)
+	MASKED, // 마스킹 표시 (대댓글 때문에 남겨둠)
 	REMOVED, // 완전 제거 (조회에서 제외)
 	MASKED_BY_ADMIN,
-	REMOVED_BY_ADMIN
+	REMOVED_BY_ADMIN;
+
+	public boolean isRemoved() {
+		return this == REMOVED || this == REMOVED_BY_ADMIN;
+	}
+
+	public boolean isMasked() {
+		return this == MASKED || this == MASKED_BY_ADMIN;
+	}
+
+	public static Set<CommentStatus> getVisibleStatuses() {
+		return Set.of(NORMAL, MASKED, MASKED_BY_ADMIN);
+	}
 }
